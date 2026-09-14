783eea8b6895c15aa2939ddb1695a4a6aadb6268
Mike Maeda <mikemaeda7@gmail.com>
2026-09-14 02:35:28 -0400

    Publish photo as direct metadata-free upload

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [{ source: "/duo", destination: "/duo-friends.jpg" }];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
